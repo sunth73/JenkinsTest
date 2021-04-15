918792c20078ddd50ea044ec2dffd49d88f425d5
sunth.sun <sunth.sun@yunzhihui.com>
2021-04-15 12:03:20 +0800

    cj

---
+++ src/main/java/com/sunth/JenkinstestApplication.java
@@ -0,0 +1,13 @@
+package com.sunth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinstestApplication.class, args);
+    }
+
+}
